88c0d7082ed3a099daf7a6a77e64c37a017409d6
va6903@gmail.com <va6903@gmail.com>
2023-05-27 12:05:31 +0530

    Second commit :- Here we create action reducre and root reducer

---
+++ src/Services/Reducers/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from "redux";
+import cartItems from "./Reducer";
+
+
+export default combineReducers({
+    cartItems
+})
---
+++ src/component/Home.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Home Component</h1>
+            <div className="cart-wrapper">
+                <div className="img-wrapper item">
+                    <img src="https://media.croma.com/image/upload/v1664009469/Croma%20Assets/Communication/Mobiles/Images/243462_7_u9hdbg.png" />
+                </div>
+                <div className="text-wrapper item">
+                    <span>
+                        I-Phone
+                    </span>
+                    <span>
+                        Price: $1000.00
+                    </span>
+                </div>
+                <div className="btn-wrapper item">
+                    <button>Add To Cart</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
